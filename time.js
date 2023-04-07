4a13bc6ef9c4ef629395fda4861e78f93e10c278
Verah Kwamboka <verahnyagaka101@gmail.com>
2023-04-07 23:30:53 +0300

    successfully did itgit add .

--- time.js
+++ time.js
@@ -1,7 +1,26 @@
 function timeConversion(s) {
     // Write your code here
      let timeArr = s.split(':');
-     return timeArr
+     let hour =timeArr[0]
+     let minutes = timeArr[1]
+     let secMeridian = timeArr[2]
+     let seconds = secMeridian.split('').splice(0,2).join('')
+     let meridian = secMeridian.split('').splice(2).join('')
+    
+    if(hour == 12 && meridian=='PM'){
+        hour == hour
+        
+    }
+    else if(hour != 12){
+        hour = parseInt(hour)+12
+        
+    }
+    else if(hour == 12 && meridian == 'AM'){
+        hour= '00'
+    }
+    
+    let convertedTime = (`${hour}:${minutes}:${seconds}`)
+     return convertedTime
    
    
 }
