efff2f6686f99ee1a08a1bc60b2643ec005bf28d
Verah Kwamboka <verahnyagaka101@gmail.com>
2023-04-08 23:41:10 +0300

    finished challemge

--- gameScores.js
+++ gameScores.js
@@ -1,6 +1,21 @@
+// let scoreArr = []
 function breakingRecords(scores) {
     // Write your code here
-    scoreArr =scores
-    scoreArr.split(':')
-    return scoreArr
+    let maxScore = scores[0];
+    let minScore = scores[0];
+    let maxBreaks = 0;
+    let minBreaks = 0;
+    
+    for (let i in scores) {
+        if (scores[i] > maxScore) {
+            maxScore = scores[i];
+            maxBreaks++;
+        }
+        if (scores[i] < minScore) {
+            minScore = scores[i];
+            minBreaks++;
+        }
+    }
+    
+    return [maxBreaks, minBreaks];
 }
